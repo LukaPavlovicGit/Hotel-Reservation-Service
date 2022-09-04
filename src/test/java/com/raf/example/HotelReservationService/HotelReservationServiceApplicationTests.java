e74141c37ffb8fc24f381371a1bbb01bf30a24e3
lukapavlovic <lpavlovic11521rn@raf.rs>
2022-09-04 02:33:37 +0200

    Initial commit

---
+++ src/test/java/com/raf/example/HotelReservationService/HotelReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.example.HotelReservationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
