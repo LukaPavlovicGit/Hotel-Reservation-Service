c52e8ed9a1334422f414b4f0ac28956cce6414f5
lukapavlovic <lpavlovic11521rn@raf.rs>
2022-09-12 01:41:01 +0200

    izmene

--- src/main/java/com/raf/example/HotelReservationService/domain/Hotel.java
+++ src/main/java/com/raf/example/HotelReservationService/domain/Hotel.java
@@ -1,6 +1,7 @@
 package com.raf.example.HotelReservationService.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +11,72 @@ public class Hotel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    private Long managerId;
+
+    @NotNull
+    private String city;
+
+    @NotNull
     private String name;
+
     private String description;
+
+    @NotNull
     private Integer numberOfRooms;
-    @OneToMany(mappedBy="Hotel")
-    private List<Reservation> reservations = new ArrayList<>();
 
-
-    public Hotel() {
+    public Hotel(Long managerId, String city, String name, String description, Integer numberOfRooms) {
+        this.managerId = managerId;
+        this.city = city;
+        this.name = name;
+        this.description = description;
+        this.numberOfRooms = numberOfRooms;
     }
+
+    public Hotel() {}
 
     public Long getId() {
         return id;
+    }
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Long managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(Integer numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
     }
 }
